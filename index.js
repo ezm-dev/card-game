b4f9746cc308984f1de03b6d095bdc69a864e5f3
ezm-dev <ezm.sabry@gmail.com>
2025-12-15 21:16:11 +1000

    Add score functionality
    Add the final winner functionality for each player, show it on the screen and determine the Final winner.

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 let deckId
+let myScore =0
+let computerScore = 0
 
+document.getElementById("draw-cards").disabled = true
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
             console.log(data)
             deckId = data.deck_id
+            document.getElementById("count").textContent =`Remaining Cards: ${data.remaining}`
+            document.getElementById("draw-cards").disabled = false
+       
         })
 }
 function handleDraw(){
+    
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
@@ -18,8 +25,23 @@ function handleDraw(){
                 <img src=${data.cards[1].image} />
             ` 
           const result = compareCards(data.cards[0],data.cards[1])
+        
           document.getElementById("result").innerHTML= result
           document.getElementById("count").textContent =`Remaining Cards: ${data.remaining}`
+        // No more card
+          if(!data.remaining){
+            document.getElementById("draw-cards").disabled = true
+            if(myScore>computerScore){
+                document.getElementById("result").innerHTML= "🎉You won the game🎉"
+            }
+            else if(myScore<computerScore){
+                 document.getElementById("result").innerHTML= "🎉Computer won the game🎉"
+            }
+            else {
+                document.getElementById("result").innerHTML= "🎉It's a tie game🎉"
+            }
+
+          }
 
           
 
@@ -40,7 +62,20 @@ function compareCards(card1, card2){
   let value2= card2.value 
 value1 = value1=="JACK"?11:value1=="QUEEN"?12:value1=="KING"?13:value1=="ACE"?100:Number(value1)
 value2 = value2=="JACK"?11:value2=="QUEEN"?12:value2=="KING"?13:value2=="ACE"?100:Number(value2)
-let  result = value1>value2?"Card1 wins!":value1<value2?"Card2 wins!":"It's a tie!"
+if (value1 > value2) {
+    result = "Computer wins!";
+    computerScore++;
+    document.getElementById("comp-score").textContent=`Computer score: ${computerScore}`
+
+} else if (value1 < value2) {
+    result = "You win!";
+    myScore++;
+    document.getElementById("my-score").textContent=`My score: ${myScore}`
+} else {
+    result = "It's a tie!";
+}
+
+
 return result
 
  
